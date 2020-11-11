8f049d28b68ebcfd3c5f0ea52f16e397550bed07
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-11 06:28:07 +0100

    feat: frontend added

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('build'))
+
 let persons = [
   {
     id: 1,
